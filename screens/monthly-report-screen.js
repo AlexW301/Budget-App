21cd14a520e91e278b5b83851b77ddb407600b7b
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-03-09 08:43:17 -0500

    Update monthly-report-screen.js

--- screens/monthly-report-screen.js
+++ screens/monthly-report-screen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { StackActions } from '@react-navigation/native';
 
 
@@ -8,21 +8,64 @@ export default function MonthlyReportScreen({ route, navigation }) {
   budgetReport = budgetArray[budgetArray.length - 1]
   amountSaved = budgetReport.budget - budgetReport.spent
   let amountSavedText
-
+  let monthNames = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
   if (amountSaved > 0) {
-    amountSavedText = `Congrats you saved $${amountSaved} last month!`
+    amountSavedText = `You saved an extra $${amountSaved} last month!`
   } else if (amountSaved === 0) {
-    amountSavedText = `Congrats you were right on budget last month!`
+    amountSavedText = `You were exactly on your budget last month!`
   } else if (amountSaved < 0) {
-    amountSavedText = `You went $${amountSaved} over budget last month. Better luck this month!`
+    amountSavedText = `You went $${amountSaved} over budget last month. You'll do better this month!`
   }
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around', backgroundColor: colors.main}}>
-      <Text style={{flex: 1 }}>{budgetReport.spent < budgetReport.budget ? `Good Job!` : ``}{( budgetReport.spent > budgetReport.budget && budgetReport.spent - budgetReport.budget) <= (budgetReport.budget * .10) ? `Good Job! You Were close` : ``}</Text>
-      <Text style={{flex: 1 }}>Last Month | {budgetReport.month}</Text>
-      <Text style={{flex: 1 }}>Your Budget was ${budgetReport.budget}</Text>
-      <Text style={{flex: 1 }}>You spent ${budgetReport.spent}</Text>
-      <Text style={{flex: 1 }}>{amountSavedText}</Text>
+      <Text style={styles.reportMonth}>Report for {monthNames[budgetReport.month.substring(0,1)]}</Text>
+      <Text style={styles.congratsText}>{budgetReport.spent < budgetReport.budget ? `Congratulations! You stayed within your budget last month!` : `` || ( budgetReport.spent > budgetReport.budget && budgetReport.spent - budgetReport.budget) <= (budgetReport.budget * .10) ? `Almost! You went just a little bit over last month.` : `You went over your budget last month...` }</Text>
+      <Text style={styles.resultsText1}>Results!</Text>
+      <Text style={styles.budgetText}>Your Budget was ${budgetReport.budget}</Text>
+      <Text style={styles.spentText}>You spent ${budgetReport.spent}</Text>
+      <Text style={styles.resultsText2}>{amountSavedText}</Text>
     </View>
+    
   )
 }
+
+const styles = StyleSheet.create({
+  reportMonth: {
+    flex: 1,
+    fontSize: 30,
+    fontFamily: "Rubik_700Bold",
+    color: colors.text,
+  },
+  congratsText: {
+    flex: 1,
+    fontSize: 22,
+    fontFamily: "Rubik_400Regular",
+    color: colors.text,
+    textAlign: 'center'
+  },
+  budgetText: {
+    flex: .5,
+    fontSize: 22,
+    fontFamily: "Rubik_400Regular",
+    color: colors.text,
+  },
+  spentText: {
+    flex: .5,
+    fontSize: 22,
+    fontFamily: "Rubik_400Regular",
+    color: colors.text,
+  },
+  resultsText1: {
+    flex: .5,
+    fontSize: 22,
+    fontFamily: "Rubik_400Regular",
+    color: colors.text,
+  },
+  resultsText2: {
+    flex: 1,
+    fontSize: 22,
+    fontFamily: "Rubik_400Regular",
+    color: colors.text,
+    textAlign: 'center'
+  }
+})
