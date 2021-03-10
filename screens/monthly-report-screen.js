4cdbb40a1ac643d9464df87dc3d64362b7c59a04
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-03-10 08:40:37 -0500

    Update monthly-report-screen.js

--- screens/monthly-report-screen.js
+++ screens/monthly-report-screen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { StackActions } from '@react-navigation/native';
+import TransactionButton from '../components/transaction-button'
 
 
 export default function MonthlyReportScreen({ route, navigation }) {
@@ -20,10 +21,14 @@ export default function MonthlyReportScreen({ route, navigation }) {
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around', backgroundColor: colors.main}}>
       <Text style={styles.reportMonth}>Report for {monthNames[budgetReport.month.substring(0,1)]}</Text>
       <Text style={styles.congratsText}>{budgetReport.spent < budgetReport.budget ? `Congratulations! You stayed within your budget last month!` : `` || ( budgetReport.spent > budgetReport.budget && budgetReport.spent - budgetReport.budget) <= (budgetReport.budget * .10) ? `Almost! You went just a little bit over last month.` : `You went over your budget last month...` }</Text>
-      <Text style={styles.resultsText1}>Results!</Text>
-      <Text style={styles.budgetText}>Your Budget was ${budgetReport.budget}</Text>
+      <View style={styles.results}>
+      <Text style={styles.resultsText1}>Your Results!</Text>
+      <Text style={styles.budgetText}>Last Month Your Budget was ${budgetReport.budget}</Text>
       <Text style={styles.spentText}>You spent ${budgetReport.spent}</Text>
       <Text style={styles.resultsText2}>{amountSavedText}</Text>
+      </View>
+      <TransactionButton style={styles.adjustBudget} title="Adjust Budget" onPress={{}}></TransactionButton>
+      <TransactionButton style={styles.keepBudget} title="Keep Same Budget" onPress={{}}></TransactionButton>
     </View>
     
   )
@@ -31,41 +36,60 @@ export default function MonthlyReportScreen({ route, navigation }) {
 
 const styles = StyleSheet.create({
   reportMonth: {
-    flex: 1,
+    flex: .05,
     fontSize: 30,
     fontFamily: "Rubik_700Bold",
     color: colors.text,
+    //marginTop: '5%',
+    //marginBottom: '-21%',
+    //backgroundColor: 'yellow'
   },
   congratsText: {
-    flex: 1,
+    flex: .1,
     fontSize: 22,
     fontFamily: "Rubik_400Regular",
     color: colors.text,
-    textAlign: 'center'
+    textAlign: 'center',
+    //backgroundColor: 'blue'
   },
   budgetText: {
     flex: .5,
     fontSize: 22,
     fontFamily: "Rubik_400Regular",
     color: colors.text,
+    //backgroundColor: 'purple'
   },
   spentText: {
     flex: .5,
     fontSize: 22,
     fontFamily: "Rubik_400Regular",
     color: colors.text,
+    //backgroundColor: 'brown'
   },
   resultsText1: {
     flex: .5,
-    fontSize: 22,
-    fontFamily: "Rubik_400Regular",
+    fontSize: 27,
+    fontFamily: "Rubik_700Bold",
     color: colors.text,
+    //backgroundColor: 'green'
   },
   resultsText2: {
     flex: 1,
     fontSize: 22,
     fontFamily: "Rubik_400Regular",
     color: colors.text,
-    textAlign: 'center'
+    textAlign: 'center',
+    //backgroundColor: 'red'
+  },
+  results: {
+    flex: .35,
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  adjustBudget: {
+    flex: .1
+  },
+  keepBudget: {
+    flex: .1
   }
 })
