a30aa14de9eadaee114f76842b36afb4790fa581
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-03-30 08:43:08 -0400

    settings

--- screens/settings-screen.js
+++ screens/settings-screen.js
@@ -3,6 +3,8 @@ import { View, Text, StyleSheet, FlatList, Alert } from 'react-native';
 import { StackActions } from '@react-navigation/native';
 import { useIsFocused } from "@react-navigation/native";
 import TouchableScale from "react-native-touchable-scale";
+import { Button } from 'react-native';
+import * as Updates from 'expo-updates';
 
 
 
@@ -12,6 +14,27 @@ export default function SettingsScreen({ route, navigation }) {
 
     const [refresh, initRefresh] = useState(1);
 
+    const resetApp = () => {
+        Alert.alert(
+            "WARNING: Do you wish to reset the app?",
+            "Doing so will delete all of your saved data. Including your history, stash, budget, transactions, etc.. Continue only if you wish to start fresh as if downloading the app for the first time",
+            [
+              {
+                text: "Yes, Delete!",
+                onPress: () => {
+                  clearAsyncStorage();
+                  // Need way to reload app for changes to show
+                },
+              },
+              {
+                text: "Cancel, Dont Reset",
+                onPress: () => {},
+                style: "cancel",
+              },
+            ],
+            { cancelable: false }
+          );
+    }
 
   if (isFocused) {
       
@@ -19,10 +42,10 @@ export default function SettingsScreen({ route, navigation }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', backgroundColor: colors.main}}>
           <Text style={styles.header}>Settings</Text>
-          <TouchableScale style={styles.item} onPress={alert('Button Pressed')}><Text style={styles.itemText}>Settings</Text></TouchableScale>
-          <TouchableScale style={styles.item} onPress={alert('Button Pressed')}><Text style={styles.itemText}>Settings</Text></TouchableScale>
-          <TouchableScale style={styles.item} onPress={alert('Button Pressed')}><Text style={styles.itemText}>Settings</Text></TouchableScale>
-          <TouchableScale style={styles.item} onPress={alert('Button Pressed')}><Text style={styles.itemText}>Settings</Text></TouchableScale>
+          <TouchableScale style={styles.item} onPress={() => {alert('button pressed')}}><Text style={styles.itemText}>Settings</Text></TouchableScale>
+          <TouchableScale style={styles.item} onPress={() => {alert('button pressed')}}><Text style={styles.itemText}>Settings</Text></TouchableScale>
+          <TouchableScale style={styles.item} onPress={() => {alert('button pressed')}}><Text style={styles.itemText}>Settings</Text></TouchableScale>
+          <TouchableScale style={styles.item} onPress={() => {resetApp();}}><Text style={styles.itemText}>Reset</Text></TouchableScale>
         </View>
       );
 }
